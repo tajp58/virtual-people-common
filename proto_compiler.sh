973e85ba998079e51689642cfd6ec8c398f06892
Patel <Taj.Patel@kantar.com>
2023-12-05 18:05:30 +0000

    adding compiled proto files

---
+++ proto_compiler.sh
@@ -0,0 +1,15 @@
+protoc -I=C:/Users/PatelTaj/OneDrive\ -\ Kantar/Documents/Project_Origin/virtual-people-common/src/main/proto/ --python_out=C:/Users/PatelTaj/OneDrive\ -\ Kantar/Documents/Project_Origin/virtual-people-common/ wfa/virtual_people/common/event.proto
+
+protoc -I=C:/Users/PatelTaj/OneDrive\ -\ Kantar/Documents/Project_Origin/virtual-people-common/src/main/proto/ --python_out=C:/Users/PatelTaj/OneDrive\ -\ Kantar/Documents/Project_Origin/virtual-people-common/ wfa/virtual_people/common/config.proto
+
+protoc -I=C:/Users/PatelTaj/OneDrive\ -\ Kantar/Documents/Project_Origin/virtual-people-common/src/main/proto/ --python_out=C:/Users/PatelTaj/OneDrive\ -\ Kantar/Documents/Project_Origin/virtual-people-common/ wfa/virtual_people/common/demographic.proto
+
+protoc -I=C:/Users/PatelTaj/OneDrive\ -\ Kantar/Documents/Project_Origin/virtual-people-common/src/main/proto/ --python_out=C:/Users/PatelTaj/OneDrive\ -\ Kantar/Documents/Project_Origin/virtual-people-common/ wfa/virtual_people/common/field_filter.proto
+
+protoc -I=C:/Users/PatelTaj/OneDrive\ -\ Kantar/Documents/Project_Origin/virtual-people-common/src/main/proto/ --python_out=C:/Users/PatelTaj/OneDrive\ -\ Kantar/Documents/Project_Origin/virtual-people-common/ wfa/virtual_people/common/geo_location.proto
+
+protoc -I=C:/Users/PatelTaj/OneDrive\ -\ Kantar/Documents/Project_Origin/virtual-people-common/src/main/proto/ --python_out=C:/Users/PatelTaj/OneDrive\ -\ Kantar/Documents/Project_Origin/virtual-people-common/ wfa/virtual_people/common/label.proto
+
+protoc -I=C:/Users/PatelTaj/OneDrive\ -\ Kantar/Documents/Project_Origin/virtual-people-common/src/main/proto/ --python_out=C:/Users/PatelTaj/OneDrive\ -\ Kantar/Documents/Project_Origin/virtual-people-common/ wfa/virtual_people/common/model.proto
+
+protoc -I=C:/Users/PatelTaj/OneDrive\ -\ Kantar/Documents/Project_Origin/virtual-people-common/src/main/proto/ --python_out=C:/Users/PatelTaj/OneDrive\ -\ Kantar/Documents/Project_Origin/virtual-people-common/ wfa/virtual_people/common/panel_data.proto
